55fff78abd3541ad137b7fc7a8b22e8053cd378e
sokiqo <princessxpea@gmail.com>
2026-05-27 08:15:52 -0500

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,196 @@
+#pokemon
+import random
+def pokemon1():
+        print((r" _..__                                    ___\n"))
+        print((r"/     `._                          ,--\"\"\"\"   `\"-.\n"))
+        print(("|         `.                    _.'\"\"/`.        |/`-.\n"))
+        print(("|           `.                ,+ `..' | |       |'...+.\n"))
+        print(("|     \\       \\              / /\\____,' '    __ `.`._,\".\n"))
+        print(("|      ..      \\           ,'  \\      .' ,-\"'  `. `.._,'`.\n"))
+        print(("|       .`.     \\         /     `-..-'  .  _.... |._      \\\n"))
+        print(("'        ' `     \\       /          ,-\"\"`.____...'  `.     \\\n"))
+        print((r"`        `.`.    \\     '         ,'   _,--------.`.  `.    L\n"))
+        print((r" `         ` `.   \\   j         /  .,' ,\"_.....`.`.`   \\   |\n"))
+        print((r"  `.        `. `.  \\  |        / ,'/ .','..... `.\\ \\|   .  |\n"))
+        print((r"    `.        `. .  \\ |       j . / ..'.,-\"\". \\ || ||   |  |\n"))
+        print((r"      `.        `.`. \\'.      | | | |||.   .,.','/ /'   |  |\n"))
+        print((r"        `-.       `. .:\\      | | | ||'`..___.'.','/   j   |\n"))
+        print((r"           `-._     `-._\\      \\'.`.`..`..__....','   ,   /\n"))
+        print((r"               `--......-\\      \\ `.`.`.......-\"'   ,'   /\n"))
+        print((r"                          `.     `. `-..____,.-\" _.'   ,'\n"))
+        print((r"                            >.     '--...___,..-'   _.\"\n"))
+        print((r"                          ,'  `--,__            _,-\"  `-.\n"))
+        print((r"                      _.-'      '   `'--------\"' `.      `-.\n"))
+        print((r"                    .'        ,'                   \\        `.\n"))
+        print((r"                   .        .'                      `.        .\n"))
+        print((r"                   |      ,'                          `._     |\n"))
+        print((r"                    `----'                               `\"--\" mh\n"))
+        print(("\n"))
+        print(("\n"))
+
+def pokemon2():
+        print((r"            ___   _,-'\"\"-.\n"))
+        print((r"          /`.  `./,\\`.    `.\n"))
+        print((r"         /'. |  / || |      .\n"))
+        print((r"        . `|,+-'| `| |       `._\n"))
+        print((r"        `,-'    `. | '          `.\n"))
+        print((r"       ,' -'      `\"'             `.\n"))
+        print((r"      /\"`-.                         `.\n"))
+        print((r"     :`.   \\                          .\n"))
+        print((r"    ' `.\\   \\                          `\n"))
+        print((r"   .`.  .`   `           `.             .\n"))
+        print((r"  / `.'  .`   .      .     \\             \\\n"))
+        print((r" /``  .`  `\\   .      \\     .             .\n"))
+        print((r"j  .\\  .:  .'  :       .    '             |\n"))
+        print((r"|`. .. ||  :|  |        ,..--`._          |\n"))
+        print((r"|:| |: :|  |:  |      ,'        |         |\n"))
+        print((r"||| :: |:  |:  |      |         |         |\n"))
+        print((r":'j '| :|  :|  |      |         |         |\n"))
+        print((r"`/ .j  ,:  :|  |     ,'         `         |\n"))
+        print((r" \\/ / . '  ',  |   .'            \\        '\n"))
+        print((r" ` , /,'  /.  j   |               .      /\n"))
+        print((r"  `.'/   /'   '   |  .             .   ,'\n"))
+        print((r"    .  .'/   /    `._/             '  /\n"))
+        print((r"     `.,'   /       |          __,' .'\n"))
+        print((r"       `-._,         `.   _,.-\" _,-'\n"))
+        print((r"           `+..____    `\"'    .'\n"))
+        print((r"          _/    |  `\"\"\"''\\    '\n"))
+        print((r"    _,.-\"'      \\        |     \\\n"))
+        print((r"_.-'             .       |      `\n"))
+        print((",                 |       |       `.\n"))
+        print(("`_          __,,-'        '         `.\n"))
+        print((r" `'\"\"'\"'\"''             .            .\n"))
+        print((r"                        |            |\n"))
+        print((r"                        '            /\n"))
+        print((r"                         `.        ,'\n"))
+        print((r"                           `-..,.-' mh\n"))
+
+def pokemon3():
+        print((r"                              __,.-\"\"\"'\"--..._.,---.\n"))
+        print((r"                          _,-'               /      `.\n"))
+        print((r"                       _.'                 ,'   ,-\"\"`.|\n"))
+        print((r"                     ,'                        / ,+\"`.;\n"))
+        print((r"                   ,'   ____                  . |_/  /\n"))
+        print((r"                  /  .\"'    `-.               ` `..-/`.  _\n"))
+        print((r"                 '  /                     .    `---'   `: `.\n"))
+        print((r"                /  .    ,-\"\"`.           .'             \\`-.`+\"\"`\"`.\n"))
+        print((r"               /   |  .' ,\"\".|               _,...,._    L |  `     `.\n"))
+        print((r"              /    |  | ._)  /     \\    _.-\"'        \"`. | j          .\n"))
+        print((r"             j     `  ' |  ,'        ,-'   ___......_   .|\"           |\n"))
+        print((r"             |         `-+'        ,'  _,\"__.---\"\"\"`-.`.||            |\n"))
+        print((r"             '                   ,'  ,'.-'    _______ `.`|\\           |\n"))
+        print((r"           .\"                   /  ,\",'   _,\"+.------+`.`:|           |\n"))
+        print((r"         ,'     .             ,' .,-'   .\".-'  _..._  ` \\''    .      ;\n"))
+        print((r"      _,+      /             ,  ,/    ,'.' .-\",.----.+ `7  `.   `._,.'\n"))
+        print((r"     /  '    ,'             .  '.   ,\".\" .:,-'__     :|j     `-.-'\n"))
+        print((r"    j    \\  /|`            ,  //   .,' ,'.' .\"__`.   ||'\n"))
+        print((r"  ,-+     `\" | \\             /.   //  /,\" ,'.'  \\ \\  |'\n"))
+        print((r" /           '  \\         : j '  //  //  .,'     || ,\"\n"))
+        print((r"/           /    \\        | :|  j.' j/   ||_.\") , ;<_\n"))
+        print((".            `-.   `.      : ||  || |.    `..-'.'.'   `'-._\n"))
+        print(("|              |     `.    | |:  :| |'        _,'          `.\n"))
+        print(("|              |      /`-._`.`:. \\' `.`..__.-'+              `\n"))
+        print(("`          .   F     ,     /\"`+-+.^+--`\"\"\"     `._            |\n"))
+        print((r"`.       |   /     /     .                       `._         /\n"))
+        print((r"  `._   _,..'    ,'      '                          `\"-....-'\n"))
+        print((r"     `\"\"        /       j\n"))
+        print((r"              ,'        |\n"))
+        print((r"             /          |\n"))
+        print((r"            j           '\n"))
+        print((r"            |          /\n"))
+        print((r"            |         /\n"))
+        print((r"            '        /\n"))
+        print((r"             `.___,.' mh\n"))
+
+level = 23
+days = 1
+tracker = 0
+name = 0
+while True:
+        if level < 25:
+                start = input("Welcome! Do you want to train, see the info, or leave? ")
+                if start == "train":
+                        move = input("You've encountered an enemy, which move do you want to use? (bubble or water sport) ")
+                        if move == "bubble":
+                                print("After a couple hits, you've defeated the enemy and leveled up!")
+                        elif move == "water sport":
+                                print("After a bit, you've defeated the enemy and leveled up!")
+                        level = level + 1
+                        days = days + 1
+                        continue
+
+                if start == "info":
+                        ask = input("What kind of info do you want to see? (pokemon, level, art) ")
+                        if ask == "pokemon":
+                                name = "Poliwag"
+                        if ask == "level":
+                                print(level)
+                        if ask == "art":
+                                pokemon1()
+                        days = days + 1
+                        continue
+
+                if start == "leave":
+                        print("Goodbye")
+                        break
+        if level >= 25:
+                days = days + 1
+                if tracker == 0:
+                        print("Your pokemon has evolved!")
+                        tracker = tracker + 1
+                question = input("Do you want to continue to train, see the info, evolve, or leave? ")
+                if question == "train":
+                        move = input("You've encountered an enemy, which move do you want to use? (bubble, water gun, and hydro pump) ")
+                        if move == "bubble":
+                                print("After a bit, you've defeated the enemy and leveled up!")
+                        elif move == "water gun":
+                                print("After a couple hits, you've defeated the enemy and leveled up!")
+                        elif move == "hydro pump":
+                                print("After a couple hits, you've defeated the enemy and leveled up!")
+                        level = level + 2
+                if question == "info":
+                        ask = input("What kind of info do you want to see? (pokemon, level, art) ")
+                        if ask == "pokemon":
+                                name = "Poliwhirl"
+                                print(name)
+                        if ask == "level":
+                                print(level)
+                        if ask == "art":
+                                pokemon2()
+                                days = days + 1
+                                continue
+                if question == "leave":
+                        print("Goodbye")
+                        break
+                if question == "evolve":
+                        if tracker == 1:
+                                print("Your pokemon has evolved!")
+                                tracker = tracker + 1
+                        if tracker == 2:
+                                question = input("Do you want to continue to train, see the info, or leave? ")
+                                if question == "train":
+                                        move = input("You've encountered an enemy, which move do you want to use? (bubble, waterfall, ice pump, and rain dance) ")
+                                        if move == "bubble":
+                                                print("After a couple hits, you've defeated the enemy and leveled up!")
+                                        elif move == "waterfall":
+                                                print("After a bit, you've defeated the enemy and leveled up!")
+                                        elif move == "ice pump":
+                                                print("After a couple hits, you've defeated the enemy and leveled up!")
+                                        elif move == "rain dance":
+                                                print("After a bit, you've defeated the enemy and leveled up!")
+                                        level = level + 3
+                                        continue
+                                if question == "info":
+                                        ask = input("What kind of info do you want to see? (pokemon, level, art) ")
+                                        if ask == "pokemon":
+                                                name = "Poliwrath"
+                                                print(name)
+                                        if ask == "level":
+                                                print(level)
+                                        if ask == "art":
+                                                pokemon3()
+                                        days = days + 1
+                                        continue
+                                if question == "leave":
+                                        print("Goodbye")
+                                        break
